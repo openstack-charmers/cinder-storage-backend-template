035528fa844d1c742653d6bc4769c531078ca223
Luciano Lo Giudice <luciano.logiudice@canonical.com>
2021-12-01 14:49:53 -0300

    Use the new cinder plugin base class
    This patchset simplifies the skeleton for future cinder plugins by
    basing the implementation on the new cinder plugin base class. With
    these changes, only a single method needs to be overriden for cinder
    classes such as this one.
    
    Depends-On: I55a849dd11e83e5b2b23efcece6ff8dfa511c96f

--- cinder-{{cookiecutter.driver_name_lc}}/src/charm.py
+++ cinder-{{cookiecutter.driver_name_lc}}/src/charm.py
@@ -18,71 +18,26 @@
 import json
 
 
-from ops_openstack.core import OSBaseCharm
+from ops_openstack.plugins.classes import BaseCinderCharm
 from ops.framework import StoredState
 from ops.main import main
 from ops.model import ActiveStatus
 
 
-class Cinder{{ cookiecutter.driver_name }}Charm(OSBaseCharm):
+class Cinder{{ cookiecutter.driver_name }}Charm(BaseCinderCharm):
 
-    _stored = StoredState()
     PACKAGES = ['{{ cookiecutter.additional_package_name|default("cinder-common", true) }}']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # We listen to the following events:
-        # - Installation: Just to set the charm status
-        # - Configuration changes: Rewrite the cinder.conf file and inform
-        #   other charms of the changes
-        # - Backend storage join/change: Inform the other charm of what the
-        #   configuration is and the backend name.
-        self.framework.observe(self.on.install, self._on_install)
-        self.framework.observe(self.on.config_changed, self._on_config)
-        self.framework.observe(
-            self.on.storage_backend_relation_joined,
-            self._on_storage_backend)
-        self.framework.observe(
-            self.on.storage_backend_relation_changed,
-            self._on_storage_backend)
 
-    def _on_install(self, _):
-        self.install_pkgs()
-        self.unit.status = ActiveStatus('Unit is ready')
-
-    def _render_config(self, config, app_name):
-        # Generate the JSON with the updated configuration.
+    def cinder_configuration(self, config):
+        # Return the configuration to be set by the principal.
         volume_driver = ''
         options = [
-            ('volume_driver', volume_driver),
+            ('volume_driver', volume_driver)
         ]
-        return json.dumps({
-            "cinder": {
-                "/etc/cinder/cinder.confg": {
-                    "sections": {app_name: options}
-                }
-            }
-        })
-
-    def _set_data(self, data, config, app_name):
-        # Inform another charm of the backend name and our configuration.
-        data['backend-name'] = config['volume-backend-name'] or app_name
-        data['subordinate_configuration'] = self._render_config(
-            config, app_name)
-
-    def _on_config(self, event):
-        config = dict(self.framework.model.config)
-        rel = self.framework.model.relations.get('storage-backend')[0]
-        app_name = self.framework.model.app.name
-        for unit in self.framework.model.get_relation('storage-backend').units:
-            self._set_data(rel.data[self.unit], config, app_name)
-        self.unit.status = ActiveStatus('Unit is ready')
-
-    def _on_storage_backend(self, event):
-        self._set_data(
-            event.relation.data[self.unit],
-            self.framework.model.config,
-            self.framework.model.app.name)
+        return options
 
 
 if __name__ == '__main__':
